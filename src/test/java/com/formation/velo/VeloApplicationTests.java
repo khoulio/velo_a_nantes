0a6913383373235a07e3475712f214bd741dc059
Souleymane Gueye <khoulio@msn.com>
2022-09-29 18:14:59 +0200

    fix mvn

--- src/test/java/com/formation/velo/VeloApplicationTests.java
+++ src/test/java/com/formation/velo/VeloApplicationTests.java
@@ -1,14 +1,12 @@
 package com.formation.velo;
 
 
-import org.junit.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VeloApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
